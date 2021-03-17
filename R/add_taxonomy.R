1219e3259c4502f13ae7627e2501edc64dc35ecd
Pierre Rogy <pierre.rogy@gmail.com>
2021-03-17 15:38:22 -0700

    fixed vignette and some typos

--- R/add_taxonomy.R
+++ R/add_taxonomy.R
@@ -2,13 +2,22 @@
 #'
 #' Add all taxonomy columns needed by the package
 #' 
-#' @param data_table The input data table
+#' @param data_table The input data table, the column 'bwg_name' has to be present 
 #' @return An updated measurement table that will be used to estimate sizes and gather taxonomy
 #' @export
 add_taxonomy <- function(data_table){
+  
+  # Function %notin%
+  '%notin%' <- 
+    Negate('%in%')
+  
+  # This one column needs to be here
+  if("bwg_name" %notin% colnames(data_table))
+    stop("Please call column with bwg species names values 'bwg_name'")
+  
   # Add taxonomy to data table
   data_table <- 
-    test %>% 
+    data_table %>% 
     dplyr::left_join(get_bwgnames(),
                      by = "bwg_name") %>% 
     dplyr::relocate(domain:species, 
--- R/pitilla_data.R
+++ R/pitilla_data.R
@@ -1,20 +1,28 @@
 #' Get practice data
 #'
 #' Gets a toy dataset, collected in Pitilla (Guanacaste, Costa Rica) by Diane Srivastava in 2002.
-#' Some mock rows added by Pierre Rogy to demosntrate some aspects of the package
+#' Some mock rows added by Pierre Rogy to demonstrate some aspects of the package
 #'
-#' @param taxo Do you want all taxonomic columns to be included? (TRUE (default)/FALSE)
+#' @param taxonomy Do you want all taxonomic columns to be included? (TRUE (default)/FALSE)
 #' @return The Pitilla data 
 #' @export
-pitilla_data <- function(taxo = T){
+pitilla_data <- function(taxonomy = TRUE){
   pitilla <- 
-    read.csv(system.file("extdata", "pitilla.csv", package = "hellometry")) %>%
+    read.csv(system.file("extdata", "pitilla.csv", 
+                         package = "hellometry"))
   
   ## If the taxonomy is not wanted  
-  if(taxo == FALSE)  
+  if(!(taxonomy))  
     pitilla <-
       pitilla %>% 
       dplyr::select(bwg_name, size_mm, stage, abundance)
+  
+  ## If taxonomy is wanted, put weird species name is species column
+  if(taxonomy)
+    c(pitilla[112:117, 14] <- pitilla[112:117, 1],
+      pitilla[112:117, 1] <- NA)
+      
+  
   ## Return data
   return(pitilla)       
   
